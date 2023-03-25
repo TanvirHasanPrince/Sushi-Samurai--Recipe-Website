f3787b2fc7b4d1ca902bc76169ba1034e625c6ff
Tanvir Hasan Prince <t.hasan.prince@gmail.com>
2023-03-26 00:47:43 +0600

    made modal

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Banner from './sections/Banner';
 import Receipies from './sections/Receipies';
+import RecipeModal from './sections/RecipeModal';
 
 function App() {
   return (
     <div className="w-[1440px] m-auto">
    <Banner></Banner>
    <Receipies></Receipies>
+   <RecipeModal></RecipeModal>
     </div>
   );
 }
