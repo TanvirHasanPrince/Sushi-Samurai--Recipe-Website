23b701b2f44d61b8897dc7e1c2d45872de39df8d
Tanvir Hasan Prince <t.hasan.prince@gmail.com>
2023-03-26 02:45:18 +0600

    modal

--- src/sections/RecipeStepsModal.js
+++ src/sections/RecipeStepsModal.js
@@ -1,20 +1,35 @@
 import React from 'react';
 
 const RecipeStepsModal = ({ recipeSteps }) => {
- const {name} = recipeSteps;
+ const {  image, name, ingredients, equipment, steps, tips } =
+   recipeSteps;
   return (
     <div>
-  
-      {/* Put this part before </body> tag */}
       <input type="checkbox" id="recipe-modal" className="modal-toggle" />
       <label htmlFor="recipe-modal" className="modal cursor-pointer">
-        <label className="modal-box relative" htmlFor="">
-          <h3 className="text-lg font-bold">
-            {name}
-          </h3>
+        <label className="modal-box relative m-auto" htmlFor="">
+          <h3 className="text-lg font-bold text-center">{name}</h3>
+          <div className="flex justify-center">
+            <img src={image} className="w-32 h-32 rounded" alt="" />
+          </div>
+   
           <p className="py-4">
-            You've been selected for a chance to get one year of subscription to
-            use Wikipedia for free!
+            <h1 className="font-bold">Ingredients</h1>
+            {ingredients}
+          </p>
+          <p className="py-4">
+            <h1 className="font-bold">Equipments</h1>
+            {equipment}
+          </p>
+
+          <p className="py-4">
+            <h1 className="font-bold">Steps</h1>
+            {steps}
+          </p>
+
+          <p className="py-4">
+            <h1 className="font-bold">Tips</h1>
+            {tips}
           </p>
         </label>
       </label>
