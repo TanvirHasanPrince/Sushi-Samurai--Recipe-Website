f9555b001a0a1e8ad5d2a5d5be982a5db54b0c62
Tanvir Hasan Prince <t.hasan.prince@gmail.com>
2023-03-25 15:36:11 +0600

    sushi samurai

--- src/sections/Banner.js
+++ src/sections/Banner.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import bannerImg from '../assets/banner.png';
+import Typewriter from "typewriter-effect";
+
 const Banner = () => {
  return (
-   <div className="flex ">
-     <div></div>
-     <div>
+   <div className="grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 p-10 ">
+     <div className='flex items-center text-5xl lg:text-9xl justify-center font-[imported]'>
+       <Typewriter
+         options={{
+           strings: [
+             "Experience the art of sushi",
+             "Fresh fish expertly crafted",
+             "Sushi that's simply irresistible",
+             "Where every roll is a work of art",
+             "Discover the flavors of Japan",
+           ],
+           autoStart: true,
+           loop: true,
+         }}
+       />
+     </div>
+     <div className=''>
        <img src={bannerImg} className="" alt="A Sushi Banner" />
      </div>
    </div>
