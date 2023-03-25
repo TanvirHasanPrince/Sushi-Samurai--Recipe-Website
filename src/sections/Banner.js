3a70b96937267417d99ccce01c2f2b283c2c958b
Tanvir Hasan Prince <t.hasan.prince@gmail.com>
2023-03-26 03:55:46 +0600

    added lottie

--- src/sections/Banner.js
+++ src/sections/Banner.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import bannerImg from '../assets/banner.png';
 import Typewriter from "typewriter-effect";
+import Lottie from "lottie-react";
+import sushiAnimation from '../assets/53947-sushi.json'
+
 
 const Banner = () => {
  return (
    <div className="grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 p-10 ">
-     <div className='flex items-center text-5xl lg:text-7xl justify-center font-[imported]'>
+     <div className="flex items-center text-5xl lg:text-7xl justify-center font-[imported]">
        <Typewriter
          options={{
            strings: [
@@ -19,8 +22,9 @@ const Banner = () => {
            loop: true,
          }}
        />
+       <Lottie animationData={sushiAnimation} loop={true} />
      </div>
-     <div className=''>
+     <div className="">
        <img src={bannerImg} className="" alt="A Sushi Banner" />
      </div>
    </div>
